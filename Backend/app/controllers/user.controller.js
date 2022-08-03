1e8b1ac5a7b8a0c198bbe1484dbc66a5a6f3b712
Romain Pasquier <romain.pasquier@efrei.net>
2022-08-03 14:21:10 +0200

    edit

--- Backend/app/controllers/user.controller.js
+++ Backend/app/controllers/user.controller.js
@@ -4,7 +4,7 @@ const User = require("../models/user.model");
 const bcrypt = require('bcryptjs');
 
 exports.test = (req, res) => {
-    res.send("hello bitcessss");
+    res.send("hello world");
 }
 
 // User Register function
@@ -13,7 +13,7 @@ exports.register = (req, res) => {
     User.findOne({mail: newUser.mail}, function (err, user) {
         if (user == null){
             //Crypter le mdp
-            //newUser.password = bcrypt.hashSync(req.body.password, 10);
+            newUser.password = bcrypt.hashSync(req.body.password, 10);
             newUser.save()
                 .then(data => {
                     res.send(data);
@@ -29,3 +29,25 @@ exports.register = (req, res) => {
         }
     })
 };
+
+// User Sign function
+exports.logIn = (req, res) => {
+    User.findOne({mail: req.body.mail}, function (err, user) {
+        if (user == null){
+            res.status(401).json({ message: 'Authentication failed. email does not exist' });
+        }
+        //verif authorization
+        else if (!user.authorization()){
+            res.status(402).json({ message: 'Authorization failed. No access'});
+        }
+        //verif password
+        else if (!user.comparePassword(req.body.password)) {
+            res.status(403).json({ message: 'Authentication failed. Wrong password.' });
+        }
+        else
+        {
+            res.send(user)
+        }
+
+    });
+}
--- Backend/app/models/user.model.js
+++ Backend/app/models/user.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 //clef secrete
-const key = "uiaezhf§è!çseufh6789GZzufhzuidfh7Z7DçGFZçZIFZFUZG31261&&&dbhzjd";
+const key = "sqetrdytufigkjbhgfifukgiomoiuxghcj";
 
 // import bcryptjs - hashing function
 const bcrypt = require('bcryptjs');
@@ -39,5 +39,43 @@ const UserSchema = mongoose.Schema({
         default: []
     }
 })
+
+//Create a Schema method to compare password
+UserSchema.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
+
+//Fonction qui génere un token qu'on vérifiera par la suite
+UserSchema.methods.generateAuthToken = function(){
+    const user = this;
+    const token = jwt.sign(
+        {
+            _id: user._id,
+            nom: user.nom,
+            prenom: user.prenom,
+            mail: user.mail,
+            password: user.password,
+            dateDeNaissance: user.dateDeNaissance,
+            local: user.local,
+            urlImage: user.urlImage,
+            subscription: user.subscription,
+            numElecteur: user.numElecteur,
+            autorisedElections: user.autorisedElections,
+            createdElections: user.createdElections,
+        }, key
+    );
+    return token;
+}
+
+//Fonction qui vérifie le token
+UserSchema.methods.authorization = function () {
+    const token = this.generateAuthToken();
+    try {
+        jwt.verify(token, key);
+        return true
+    } catch (err) {
+        return false;
+    }
+}
 
 module.exports = mongoose.model("User", UserSchema);
